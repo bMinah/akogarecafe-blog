e5779babe2311d736ba15905d8b2c1497671d6c5
Joey Rattana <rattanajoey@gmail.com>
2021-08-18 11:47:58 -0700

    move client into client folder and creat api folder, create express server and nodejs mongodb connection

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true, 
+    useCreateIndex: true
+}).then(console.log("Connected to MONGODB")).catch((err) => console.log(err));
+
+app.listen("5000", () =>{
+    console.log("Backened is running.");
+})
