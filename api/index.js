22470757ea7ed5e2729ad847f540022b67c67cad
Joey Rattana <rattanajoey@gmail.com>
2021-08-18 14:21:45 -0700

    nodejs mongodb login and register routes

--- api/index.js
+++ api/index.js
@@ -2,14 +2,18 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     useCreateIndex: true
 }).then(console.log("Connected to MONGODB")).catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen("5000", () =>{
     console.log("Backened is running.");
